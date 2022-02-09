110bc270c5def008582fc810650746e9125583c1
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-02-09 10:30:39 -0600

    updating worksheets

--- code.js
+++ code.js
@@ -1,20 +1,26 @@
-// helloWorld function
+// helloWorld function //
 const helloWorld = function() {
     return "Hello, World!";
 }
+// SayHello test function //
 const sayHello = function(name) {
     if(typeof name !== "string"){
         return "Hello, World";
     }
     return "Hello, " + name;
 }
-
+// IsFive all Green see tests.js to confirm //
 function isFive(input){
     if(typeof input === "number"){
         return (Number(input)===5)
     }else return false;
 }
-
-function isEven(){
-    return "function"
+// isEven input should be opened with no fixed structured input //
+function isEven(input) {
+    if (typeof input === "number" && (input) % 2 === 0){
+        return true
+    }else
+     return false
 }
+
+//
